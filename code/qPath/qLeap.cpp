3c819df743bdf95210588d9487775ce253d5c879
2j4 <jasonihappy@gmail.com>
2024-04-13 20:28:42 +0800

    QPath and QLeap

---
+++ code/qPath/qLeap.cpp
@@ -0,0 +1,279 @@
+#include"header.h"
+#include"formula.cpp"
+#include"../parameter.cpp"
+#define BETA 0.00438471
+using namespace std;
+vector<Node>qNode;
+unordered_map<pair<int, int>, double, pairHash>disTable;
+vector<vector<int>>kSP;
+vector<acceptPath>acPaths;
+int routingReq, numQn, minCost, maxCost;
+struct CompareVector {
+    bool operator()(const vector<int>& a, const vector<int>& b) const {
+        if (a.size() == b.size()) {
+            return a > b; // 如果大小相等，按字典序比较
+        }
+        return a.size() > b.size(); // 首先基于大小比较
+    }
+};
+void input(int num, void* fileName);
+void printDisTable();
+void printGraph();
+void printSP();
+void printKSP();
+void printPath(vector<int> &path);
+void printACP();
+void printPurifiTable();
+void printNodeInfo(){
+  for(int i=0; i<numQn; i++){
+    cout << "node " << i << " x " << qNode[i].x << " y " << qNode[i].y << " neighbor " << qNode[i].neighbor.size() << '\n';
+    for(int j=0; j<qNode[i].neighbor.size(); j++){
+      cout << qNode[i].neighbor[j].to << " ";
+    }
+    cout << '\n';
+
+  }
+}
+void buildDisTable(){
+  for(int i=0; i<numQn-1; i++){
+    disTable[make_pair(i, i+1)] = distence(qNode[i], qNode[i+1]);
+  }
+  for(int i=0; i<numQn-2; i++){
+    for(int j=i+2; j<numQn; j++){
+      double tmpDis = 0;
+      for(int k=i; k<j; k++){
+        tmpDis += disTable[{k, k+1}];
+      }
+      disTable[make_pair(i, j)] = tmpDis;
+    }
+  }
+  // !! distence 需要重算，要透過通道
+  // 計算相鄰點的，在加上去
+}
+void buildGraph(){
+  for(int i=0; i<numQn; i++){
+    for(int j=i+1; j<numQn; j++){
+
+      double curProb = entangle_success_prob(disTable[{i, j}]);
+      double enProb = curProb;
+      double curFidelity = entangle_fidelity(disTable[{i, j}], BETA); // define
+      double enFidelity = curFidelity;
+      int edgeIdx = qNode[i].neighbor.size();
+      int edgeIdx2 = qNode[j].neighbor.size();
+      vector<double>fi, prb;
+      fi.push_back(curFidelity);
+      prb.push_back(curProb);
+      qNode[i].addNeighbor({edgeIdx, i, j, curProb, curFidelity, fi, prb}); // 只是紀錄可以連，實際上沒有 entangle，所以 memUsed = 0
+      // qNode[j].addNeighbor({edgeIdx2, j, i, curProb, curFidelity, fi, prb}); 答案會變差
+      
+      // 計算多次 purification 的 fidelity, probability
+      double maxF = 0; 
+      vector<double>tmpPurFi, tmpPurPrb;
+      tmpPurFi.push_back(curFidelity);
+      tmpPurPrb.push_back(curProb);
+      double tmpF = curFidelity, tmpP = curProb;
+      for(int purMem=1; purMem<=qNode[i].mem && purMem<qNode[j].mem; purMem++){
+        double purP = purify_success_prob(tmpF, enFidelity) * enProb * tmpP; 
+        double purF = purify_fidelity(tmpF, enFidelity);  
+        tmpPurFi.push_back(purF);
+        tmpPurPrb.push_back(purP);
+        maxF = max(maxF, purF);
+        tmpF = purF; // update fidelity
+        tmpP = purP; // update probability
+      }
+      // Delete all edges(u,v) from G, if Fpur max(u,v)<Fth;
+      if(maxF >= threshold){
+        // add path
+        qNode[i].neighbor[edgeIdx].purFidelity = tmpPurFi;
+        qNode[i].neighbor[edgeIdx].purProb = tmpPurPrb;
+      }
+    }
+  }
+}
+
+pair<double, double> countFB(vector<int>path, vector<int>purTimes){
+  pair<double, double>cur = {1, 1};
+  for(int i=0; i<path.size()-1; i++){
+    int curNode = path[i];
+    int nextNode = path[i+1];
+    int edgeIdx = qNode[curNode].findEdge(curNode, nextNode);
+    cur.first *= qNode[curNode].neighbor[edgeIdx].purFidelity[purTimes[i]];
+    cur.second *= qNode[curNode].neighbor[edgeIdx].purProb[purTimes[i]];
+    if(i > 0){
+      double swappingProb = swapping_success_prob();
+      cur.second *= swappingProb;
+    }
+  }
+  return cur;
+}
+
+vector<int> extendDijkstra(int src, int dest) {
+  vector<double> maxFidelity(qNode.size(), 0.0);
+  vector<int> parent(qNode.size(), -1);
+  auto cmp = [&maxFidelity](int l, int r) { return maxFidelity[l] < maxFidelity[r]; };
+  priority_queue<int, vector<int>, decltype(cmp)> queue(cmp);
+
+  maxFidelity[src] = 1.0;
+  queue.push(src);
+
+  while (!queue.empty()) {
+    int curNode = queue.top();
+    queue.pop();
+
+    if (curNode == dest) {
+      break; 
+    }
+
+    for (auto& edge : qNode[curNode].neighbor) {
+      if (edge.canUse) {
+        double newFidelity = maxFidelity[curNode] * edge.fidelity;
+
+        if (newFidelity > maxFidelity[edge.to]) {
+            maxFidelity[edge.to] = newFidelity;
+            parent[edge.to] = curNode;
+            queue.push(edge.to);
+        }
+      }
+    }
+  }
+
+  vector<int> path;
+  for (int at = dest; at != -1; at = parent[at]) {
+    path.push_back(at);
+  }
+  reverse(path.begin(), path.end());
+  if (!path.empty() && path[0] == src) {
+    return path;
+  }
+
+  return {}; 
+}
+
+void updEdgeCost(){
+  auto path = kSP[0];
+  int len = path.size();
+  vector<int>purTimes(len, 0);
+  double minEdgeF = pow(threshold, 1/(len-1));
+  bool fl = 0;
+  for(int i=0; i<len-1; i++){
+    int edgeIdx = qNode[path[i]].findEdge(path[i], path[i+1]); 
+    double curEdgeF = qNode[path[i]].neighbor[edgeIdx].purFidelity[purTimes[i]];
+    while(curEdgeF < minEdgeF){
+      purTimes[i]++;
+      if(purTimes[i] >= qNode[path[i]].neighbor[edgeIdx].purFidelity.size()){
+        fl = 1;
+        break;
+      }
+      curEdgeF = qNode[path[i]].neighbor[edgeIdx].purFidelity[purTimes[i]];
+    }
+  }
+  if(fl){
+    return;
+  }
+  auto cur = countFB(path, purTimes);
+  acPaths.push_back({cur.first, cur.second, path, purTimes});
+}
+
+void routing(){
+  kSP.push_back(extendDijkstra(0, numQn-1));
+  // printPath(kSP[0]);
+  updEdgeCost();
+}
+void init(){
+  buildDisTable();
+  // printDisTable();
+  buildGraph(); 
+  // 4 Q←Priority queue according to value of min_cost;
+  // 5 Find shortest path on G with H(min) by using BFS;
+}
+void input(){
+  // node limit 70
+  // test tle 
+  double inputX, inputY, inputEnProb;
+  int inputMem;
+  cin >> numQn;
+  for(int i=0; i<numQn; i++){
+    cin >> inputX >> inputY >> inputMem >> inputEnProb;
+    qNode.push_back({i, inputX, inputY, inputMem, inputEnProb});
+  }
+}
+int main(int argc, char* argv[]){
+  if(freopen(argv[1], "r", stdin) == nullptr){
+    cout << argv[1] << " File Open Error" << '\n';
+  }
+  // 紀錄算法實際執行時間
+  // 輸出格式 : path
+  input();
+  fclose(stdin);
+  double START, END; START = clock();
+  init();
+  routing(); 
+  printACP();
+  END = clock();  
+  cout << "Time: " << (END-START) << "ms" << '\n';
+}
+void printPath(vector<int> &path){
+	for (auto &p : path){
+		cout << p << "  ";
+	}
+	cout << endl;
+}
+void printDisTable(){
+  for(int i=0; i<numQn; i++){
+    for(int j=0; j<numQn; j++){
+      cout << "Node " << i  << " to " << j << " : "<< disTable[{i, j}] << "\n";
+    }
+    cout << '\n';
+  }
+}
+void printGraph(){
+  for(int i=0; i<numQn; i++){
+    cout << "Node " << qNode[i].id << ":\n";
+    for(auto it=qNode[i].neighbor.begin(); it!=qNode[i].neighbor.end(); it++){
+      cout << "(" << it->from << ", " << it->to << ")\nprob: " << it->prob << "\nfidelity: " << it->fidelity << "\n";
+      cout << "purFidelity: ";
+      for(auto it2=it->purFidelity.begin(); it2!=it->purFidelity.end(); it2++){
+        cout << *it2 << " ";
+      }
+      cout << '\n';
+    }
+    cout << '\n';
+  }
+}
+void printKSP(){
+  for(int i=0; i<kSP.size(); i++){
+    cout << "kSP " << i << ": ";
+    for(auto x:kSP[i]){
+      cout << x << " ";
+    }
+  }
+}
+void printACP(){
+  for(auto x:acPaths){
+    cout << "Path: ";
+    for(auto y:x.path){
+      cout << y << " ";
+    }
+    cout << '\n';
+    cout << "PurTimes: ";
+    for(int i=0; i<x.path.size()-1; i++){
+      cout << x.purTimes[i] << " ";
+    }
+    cout << '\n';
+    auto tmp = countFB(x.path, x.purTimes);
+    cout << "Fidelity: " << tmp.first << '\n';
+    cout << "Probability: " << tmp.second << '\n';
+    cout << '\n';
+  }
+}
+void printPurifiTable(){
+  for(int i=0; i<numQn; i++){
+    for(auto nxt: qNode[i].neighbor){
+      cout << "Node " << i << " to " << nxt.to << " : ";
+      for(int j=0; j<nxt.purFidelity.size(); j++){
+        cout << "pur times = " << j << "\nF = " << nxt.purFidelity[j] << "\n";
+      }
+    }
+    cout << '\n';
+  }
+}
---
+++ code/qPath/qPath.cpp
@@ -0,0 +1,426 @@
+#include"header.h"
+#include"formula.cpp"
+#include"../parameter.cpp"
+#define BETA 0.00438471
+using namespace std;
+vector<Node>qNode;
+unordered_map<pair<int, int>, double, pairHash>disTable;
+vector<vector<int>>kSP;
+vector<acceptPath>acPaths;
+int routingReq, numQn, minCost, maxCost;
+struct CompareVector {
+  bool operator()(const vector<int>& a, const vector<int>& b) const {
+    if (a.size() == b.size()) {
+      return a > b; // 如果大小相等，按字典序比较
+    }
+    return a.size() > b.size(); // 首先基于大小比较
+  }
+};
+void input(int num, void* fileName);
+void printDisTable();
+void printGraph();
+void printSP();
+void printALLACP();
+void printKSP();
+void printPath(vector<int> &path);
+void printACP();
+void printPurifiTable();
+void printNodeInfo(){
+  for(int i=0; i<numQn; i++){
+    cout << "node " << i << " x " << qNode[i].x << " y " << qNode[i].y << " neighbor " << qNode[i].neighbor.size() << '\n';
+    for(int j=0; j<qNode[i].neighbor.size(); j++){
+      cout << qNode[i].neighbor[j].to << " ";
+    }
+    cout << '\n';
+
+  }
+}
+void buildDisTable(){
+  for(int i=0; i<numQn-1; i++){
+    disTable[make_pair(i, i+1)] = distence(qNode[i], qNode[i+1]);
+  }
+  for(int i=0; i<numQn-2; i++){
+    for(int j=i+2; j<numQn; j++){
+      double tmpDis = 0;
+      for(int k=i; k<j; k++){
+        tmpDis += disTable[{k, k+1}];
+      }
+      disTable[make_pair(i, j)] = tmpDis;
+    }
+  }
+}
+void buildGraph(){
+  for(int i=0; i<numQn; i++){
+    for(int j=i+1; j<numQn; j++){
+      double curProb = entangle_success_prob(disTable[{i, j}]);
+      double enProb = curProb;
+      double curFidelity = entangle_fidelity(disTable[{i, j}], BETA); // define
+      double enFidelity = curFidelity;
+      int edgeIdx = qNode[i].neighbor.size();
+      int edgeIdx2 = qNode[j].neighbor.size();
+      vector<double>fi, prb;
+      fi.push_back(curFidelity);
+      prb.push_back(curProb);
+      qNode[i].addNeighbor({edgeIdx, i, j, curProb, curFidelity, fi, prb}); // 只是紀錄可以連，實際上沒有 entangle，所以 memUsed = 0
+      // qNode[j].addNeighbor({edgeIdx2, j, i, curProb, curFidelity, fi, prb}); 答案會變差
+      
+      // 計算多次 purification 的 fidelity, probability
+      double maxF = 0; 
+      vector<double>tmpPurFi, tmpPurPrb;
+      tmpPurFi.push_back(curFidelity);
+      tmpPurPrb.push_back(curProb);
+      double tmpF = curFidelity, tmpP = curProb;
+      for(int purMem=1; purMem<=qNode[i].mem && purMem<=qNode[j].mem; purMem++){
+        double purP = purify_success_prob(tmpF, enFidelity) * enProb * tmpP; 
+        double purF = purify_fidelity(tmpF, enFidelity);  
+        tmpPurFi.push_back(purF);
+        tmpPurPrb.push_back(purP);
+        maxF = max(maxF, purF);
+        tmpF = purF; // update fidelity
+        tmpP = purP; // update probability
+      }
+      // Delete all edges(u,v) from G, if Fpur max(u,v)<Fth;
+      if(maxF >= threshold){
+        // add path
+        qNode[i].neighbor[edgeIdx].purFidelity = tmpPurFi;
+        qNode[i].neighbor[edgeIdx].purProb = tmpPurPrb;
+      } 
+      // else {
+      //   vector<double>tmp0(min(qNode[i].mem, qNode[j].mem), 0);
+      //   qNode[i].neighbor[edgeIdx].purFidelity = tmp0;
+      //   qNode[i].neighbor[edgeIdx].purProb = tmp0;
+      // }
+    }
+  }
+  // printPurifiTable();
+}
+
+pair<double, double> countFB(vector<int>path, vector<int>purTimes){
+  pair<double, double>cur = {1, 1};
+  bool memOverFlow = 0;
+  int lastNodeMemUsed = 0;
+  for(int i=0; i<path.size()-1; i++){
+    int curNode = path[i];
+    int nextNode = path[i+1];
+    int edgeIdx = qNode[curNode].findEdge(curNode, nextNode);
+    int curNodeMemUsed = purTimes[i];
+    if(curNodeMemUsed + lastNodeMemUsed > qNode[curNode].mem){
+      memOverFlow = 1;
+      break;
+    }
+    // ! 
+    if(purTimes[i] >= qNode[curNode].neighbor[edgeIdx].purFidelity.size()){
+      cout << "node " << curNode << " to " << nextNode << '\n';
+      cout << purTimes[i] << " " << qNode[curNode].neighbor[edgeIdx].purFidelity.size() << '\n';
+      for(auto x:qNode[curNode].neighbor[edgeIdx].purFidelity){
+        cout << x << " ";
+      }
+      cout << '\n';
+    }
+    assert(purTimes[i] < qNode[curNode].neighbor[edgeIdx].purFidelity.size());
+    cur.first *= qNode[curNode].neighbor[edgeIdx].purFidelity[purTimes[i]];
+    cur.second *= qNode[curNode].neighbor[edgeIdx].purProb[purTimes[i]];
+    if(i > 0){
+      cur.second *= qNode[i].swappingProb;
+    }
+    lastNodeMemUsed = curNodeMemUsed;
+  }
+  if(memOverFlow){
+    return {0, 0};
+  }
+  return cur;
+}
+
+vector<int> bfsSP(int s, int d){
+	// return shortest path with s->d
+	queue<int> que;
+	vector<bool> vis(numQn, 0);
+	vector<int> par(numQn, -1);
+	vector<int> ret;
+	que.push(s);
+	int cur;
+	while (!que.empty()){
+		cur = que.front();
+		que.pop();
+		if (cur == d){
+			break;
+		}
+		for (auto it = qNode[cur].neighbor.begin(); it != qNode[cur].neighbor.end(); it++){
+			if (!vis[it->to] && it->canUse == 1){
+				vis[it->to] = 1;
+				par[it->to] = cur;
+				que.push(it->to);
+			}
+		}
+	}
+	if (cur != d)
+		return {};
+	while (cur != s){
+		ret.push_back(cur);
+		cur = par[cur];
+	}
+	ret.push_back(s);
+	reverse(ret.begin(), ret.end());
+	return ret;
+}
+
+vector<vector<int>> yenKSP(int src, int dest, int K, int H){
+	vector<vector<int>> A; // KSP set
+	priority_queue<vector<int>, vector<vector<int>>, CompareVector> B;
+
+	vector<int> initialPath = bfsSP(src, dest);
+	if (initialPath.empty()){
+		return A;
+	}
+
+	A.push_back(initialPath);
+
+	bool overSize = 0;
+
+	for (int k = 1; k < K && !overSize; ++k){
+		for (size_t i = 0; i < A[k - 1].size() - 1 && !overSize; ++i){
+			// store origin statue (canUse)
+			vector<Node> qNodeBackup = qNode;
+			int spurNode = A[k - 1][i];
+			vector<int> rootPath(A[k - 1].begin(), A[k - 1].begin() + i + 1);
+
+			for (auto &path : A){
+				if (rootPath.size() < path.size() && equal(rootPath.begin(), rootPath.end(), path.begin())){
+					int edgeIdx = qNode[path[i]].findEdge(path[i], path[i + 1]);
+					qNode[path[i]].neighbor[edgeIdx].canUse = 0;
+				}
+			}
+
+			vector<int> spurPath = bfsSP(spurNode, dest);
+
+			if (!spurPath.empty()){
+				vector<int> totalPath = rootPath;
+				totalPath.insert(totalPath.end(), spurPath.begin() + 1, spurPath.end()); // prevent add multiple spurNode
+        B.push(totalPath);
+			}
+
+			// recover
+			qNode = qNodeBackup;
+		}
+
+		if (B.empty())
+			break;
+
+		A.push_back(B.top());
+		B.pop();
+	}
+
+	return A;
+}
+void updEdgeCost(){
+  // 如果 path Fidelity 沒過，就不斷找 purify 後 F 差異最大的進行 purify。
+  for(int i=0; i<kSP.size(); i++){
+    vector<int>path = kSP[i];
+    vector<int>purTimes(path.size()+1, 0);
+    auto tmp = countFB(path, purTimes);
+    double curF = tmp.first, curP = tmp.second;
+    priority_queue<pair<double, int>, vector<pair<double, int>>> purDicision;
+    // !!
+    // 
+    bool pathFailed = 0;
+    while(curF < threshold && curF){
+      double purIdx = 0, diff=0;
+      for(int i=0; i<path.size()-1; i++){
+        int curNode = path[i];
+        int nextNode = path[i+1];
+        int edgeIdx = qNode[curNode].findEdge(curNode, nextNode);
+        vector<double>edgePurF = qNode[curNode].neighbor[edgeIdx].purFidelity;
+        assert(edgePurF == qNode[curNode].neighbor[edgeIdx].purFidelity);
+        if(purTimes[i] < edgePurF.size()-1){ 
+          purDicision.push({edgePurF[purTimes[i+1]] - edgePurF[purTimes[i]], i}); // push i, i is edge of path[i]->path[i+1]
+          // if(edgePurF[purTimes[i+1]] - edgePurF[purTimes[i]] > diff){
+          //   diff = edgePurF[purTimes[i+1]]-edgePurF[purTimes[i]];
+          //   purIdx = i;
+          // }
+        }  
+      }  
+      // update fidelity, prb    
+      bool memOverFlow = 1;
+      int edgeChoose = -1; // node a -> node b
+      while(memOverFlow && !purDicision.empty()){
+        edgeChoose = purDicision.top().second;
+        purDicision.pop();
+        int curNode = path[edgeChoose];
+        int nextNode = path[edgeChoose+1];
+        int edgeIdx = qNode[curNode].findEdge(curNode, nextNode);
+        vector<double>edgePurF = qNode[curNode].neighbor[edgeIdx].purFidelity;
+        
+        int leftNodePurTimes = 0, rightNodePurTimes = purTimes[edgeChoose+1];
+        int nextRightNodePurTimes = 0;
+
+        if(edgeChoose != 0){
+          leftNodePurTimes = purTimes[edgeChoose-1];
+        }
+        if(edgeChoose != path.size()-2){ // next node mem used should consider its right node
+          nextRightNodePurTimes = purTimes[edgeChoose+2];
+        }
+
+        int curNodeMemUsed = purTimes[edgeChoose] + leftNodePurTimes + rightNodePurTimes;
+        int nextNodeMemUsed = purTimes[edgeChoose+1] + nextRightNodePurTimes + rightNodePurTimes;
+        int lNode = path[edgeChoose], rNode = path[edgeChoose+1];
+        int edgeId = qNode[lNode].findEdge(lNode, rNode);
+      
+        assert(qNode[lNode].mem >= qNode[lNode].neighbor[edgeId].purFidelity.size()-1); //!!
+        if(curNodeMemUsed <= qNode[path[edgeChoose]].mem && nextNodeMemUsed <= qNode[path[edgeChoose+1]].mem && \
+            purTimes[edgeChoose] < edgePurF.size()-1){ //? why -1 ?
+          purTimes[edgeChoose]++;
+          auto res = countFB(path, purTimes);
+          curF = res.first;
+          curP = res.second;
+          memOverFlow = 0;
+          break;
+        }
+      }   
+      if(memOverFlow && purDicision.empty()){
+        pathFailed = 1;
+        break;
+      }      
+    } 
+    if(curF >= threshold && !pathFailed){
+      acPaths.push_back({curF, curP, kSP[i], purTimes});
+    }
+  }
+}
+void delBKSP(int h){
+  vector<vector<int>>tmp;
+  for(int i=0; i<kSP.size(); i++){
+    if(kSP[i].size() == h){
+      tmp.push_back(kSP[i]);
+    }
+  }
+  kSP = tmp;
+}
+
+void routing(){
+  minCost = bfsSP(0, numQn-1).size();
+  maxCost = numQn+1;
+  for(int i=minCost; i<maxCost; i++){
+    if(!kSP.empty())
+      kSP.clear();
+    kSP = yenKSP(0, numQn-1, numQn*2, i);
+    // 不能限制 k 剛好是指定長度，可能會漏，但我猜應該只要多找一點在砍掉就可以了，另外注意 i 是 node 數量不是 hop 數
+    delBKSP(i);
+    updEdgeCost();
+  }  
+}
+void init(){
+  buildDisTable();
+  // printDisTable();
+  buildGraph(); 
+  // 4 Q←Priority queue according to value of min_cost;
+  // 5 Find shortest path on G with H(min) by using BFS;
+}
+void input(){
+  // node limit 70
+  // test tle 
+  double inputX, inputY, inputSwProb;
+  int inputMem;
+  cin >> numQn;
+  for(int i=0; i<numQn; i++){
+    cin >> inputX >> inputY >> inputMem >> inputSwProb;
+    qNode.push_back({i, inputX, inputY, inputMem, inputSwProb});
+  }
+}
+int main(int argc, char* argv[]){
+  if(freopen(argv[1], "r", stdin) == nullptr){
+    cout << argv[1] << " File Open Error" << '\n';
+  }
+  double START,END; START = clock();
+  input();
+  fclose(stdin);
+  init();
+  routing(); 
+  sort(acPaths.begin(), acPaths.end());
+  printALLACP();
+  END = clock();
+  cout << "Time: " << (END-START)/CLOCKS_PER_SEC << "s\n";
+}
+void printPath(vector<int> &path){
+	for (auto &p : path){
+		cout << p << "  ";
+	}
+	cout << endl;
+}
+void printDisTable(){
+  for(int i=0; i<numQn; i++){
+    for(int j=0; j<numQn; j++){
+      cout << "Node " << i  << " to " << j << " : "<< disTable[{i, j}] << "\n";
+    }
+    cout << '\n';
+  }
+}
+void printGraph(){
+  for(int i=0; i<numQn; i++){
+    cout << "Node " << qNode[i].id << ":\n";
+    for(auto it=qNode[i].neighbor.begin(); it!=qNode[i].neighbor.end(); it++){
+      cout << "(" << it->from << ", " << it->to << ")\nprob: " << it->prob << "\nfidelity: " << it->fidelity << "\n";
+      cout << "purFidelity: ";
+      for(auto it2=it->purFidelity.begin(); it2!=it->purFidelity.end(); it2++){
+        cout << *it2 << " ";
+      }
+      cout << '\n';
+    }
+    cout << '\n';
+  }
+}
+void printKSP(){
+  for(int i=0; i<kSP.size(); i++){
+    cout << "kSP " << i << ": ";
+    for(auto x:kSP[i]){
+      cout << x << " ";
+    }
+    cout << '\n';
+  }
+}
+void printACP(){
+  auto x = acPaths[0];
+  cout << "Path: ";
+  for(auto y:x.path){
+    cout << y << " ";
+  }
+  cout << '\n';
+  cout << "PurTimes: ";
+  for(int i=0; i<x.path.size()-1; i++){
+    cout << x.purTimes[i] << " ";
+  }
+  cout << '\n';
+  auto tmp = countFB(x.path, x.purTimes);
+  cout << "Fidelity: " << tmp.first << '\n';
+  cout << "Probability: " << tmp.second << '\n';
+}
+void printALLACP(){
+  for(int i=0; i<acPaths.size(); i++){
+    auto x = acPaths[i];
+    cout << "Path: ";
+    for(auto y:x.path){
+      cout << y << " ";
+    }
+    cout << '\n';
+    cout << "PurTimes: ";
+    for(int i=0; i<x.path.size()-1; i++){
+      cout << x.purTimes[i] << " ";
+    }
+    cout << '\n';
+    auto tmp = countFB(x.path, x.purTimes);
+    cout << "Fidelity: " << tmp.first << '\n';
+    cout << "Probability: " << tmp.second << '\n';
+  }
+}
+void printPurifiTable(){
+  for(int i=0; i<numQn; i++){
+    for(auto nxt: qNode[i].neighbor){
+      cout << "Node " << i << " to " << nxt.to << " : ";
+      cout << "Node " << i << " mem " << qNode[i].mem << '\n';
+      cout << "Node " << nxt.to << " mem " << qNode[nxt.to].mem << '\n';
+      for(int j=0; j<nxt.purFidelity.size(); j++){
+        cout << "pur times = " << j << "\nF = " << nxt.purFidelity[j] << "\n";
+      }
+    }
+    cout << '\n';
+  }
+}
